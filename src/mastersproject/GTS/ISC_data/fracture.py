d1c79b9d558d0bc5db5929e47182f642e0b138c2
Haakon Ludvig Langeland Ervik <her011@uib.no>
2019-12-20 13:20:05 +0100

    fix: Add path as parameter

--- src/mastersproject/GTS/ISC_data/fracture.py
+++ src/mastersproject/GTS/ISC_data/fracture.py
@@ -5,7 +5,7 @@ import GTS as gts
 import porepy as pp
 
 
-def convex_plane(shearzone, coords='gts'):
+def convex_plane(shearzone, coords='gts', path=None):
     """ Calculate shear-zone planes from shear-zones.
 
     Given a reference to a shear-zone, calculate the best-fit plane.
@@ -13,12 +13,13 @@ def convex_plane(shearzone, coords='gts'):
     Parameters:
     shearzone (str or list): Names of shearzones to construct planes of (S1_1, S1_2, ...)
     coords (str, optional): Coordinate system to use. Default: 'gts'.
+    path (pathlib.Path): Path/to/01BasicInputData/
 
     Returns:
     dict: the pointcloud projected to a convex, fitted plane.
 
     """
-    isc = gts.ISCData()
+    isc = gts.ISCData(path=path)
 
     if isinstance(shearzone, str):
         shearzone = [shearzone]
@@ -38,7 +39,7 @@ def convex_plane(shearzone, coords='gts'):
     return shearzones
 
 
-def fracture_network(shearzone, export: bool = False, **network_kwargs):
+def fracture_network(shearzone, export: bool = False, path=None, **network_kwargs):
     """ Make a fracture network from a selection of shear-zones.
 
     Parameters:
@@ -46,13 +47,15 @@ def fracture_network(shearzone, export: bool = False, **network_kwargs):
             if 'None': use all known shearzones.
         export (bool): Export network to vtk.
         network_kwargs: export kwargs
+            domain (dict): domain boundaries, dict of 'xmin', 'xmax', ...
+        path (pathlib.Path): Path/to/01BasicInputData/
     """
     if isinstance(shearzone, str):
         shearzone = [shearzone]
     elif shearzone is None:
         shearzone = ['S1_1', 'S1_2', 'S1_3', 'S3_1', 'S3_2']
 
-    convex = convex_plane(shearzone, coords='gts')
+    convex = convex_plane(shearzone, coords='gts', path=path)
 
     fractures = [pp.Fracture(convex[sz]['vertices']) for sz in shearzone]
     network = pp.FractureNetwork3d(fractures)
@@ -64,7 +67,7 @@ def fracture_network(shearzone, export: bool = False, **network_kwargs):
     name = network_kwargs.get('name', None)
     if export:
         if name is None:
-            name = 'fracture_network.vtu'
+            name = 'NEW_fracture_network.vtu'
         if name[-4:] != '.vtu':
             name = name + '.vtu'
         network.to_vtk(name)
