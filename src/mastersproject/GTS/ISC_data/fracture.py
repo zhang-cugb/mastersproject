0433ce888ded837e97db2521a37e880a2fc53555
Haakon Ludvig Langeland Ervik <her011@uib.no>
2019-12-23 18:54:25 +0100

    feat: Correct fracture network
    Visually confirmed that correct fracture network is generated.
    Also confirmed by low summed relative errors
    TODO: Remove comments, update function names

--- src/mastersproject/GTS/ISC_data/fracture.py
+++ src/mastersproject/GTS/ISC_data/fracture.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
+from pathlib import Path
 
 import GTS as gts
 import porepy as pp
-
 
 
 def convex_plane(shearzone, coords='gts', path=None):
@@ -77,7 +77,7 @@ def fracture_network(shearzone, export: bool = False, path=None, **network_kwarg
     return network
 
 
-def convex_plane2(shearzone_names, coord_system='gts'):
+def convex_plane2(shearzone_names, coord_system='gts', path=None):
     """ Compute vertices for the convex polygon of the projected point cloud
     to the plane of best fit for each shear-zone is shearzone_names.
 
@@ -90,13 +90,15 @@ def convex_plane2(shearzone_names, coord_system='gts'):
     coord_system : str, Default: 'gts'
         Name of coordinate system to use
         Input values: Either 'gts' or 'swiss'
+    path : pathlib.Path
+        Path/to/01BasicInputData/
 
     Returns:
     convex_shearzones : pd.DataFrame
         Convex polygon of projected points to best fit plane
 
     """
-    isc = gts.ISCData()
+    isc = gts.ISCData(path=path)
 
     if isinstance(shearzone_names, str):
         shearzone_names = [shearzone_names]
@@ -118,7 +120,7 @@ def convex_plane2(shearzone_names, coord_system='gts'):
     return df
 
 
-def fracture_network2(shearzone_names, export: bool = False, **network_kwargs):
+def fracture_network2(shearzone_names, export: bool = False, path=None, **network_kwargs):
     """ Make a fracture network from a selection of shear-zones.
 
     Parameters:
@@ -127,28 +129,38 @@ def fracture_network2(shearzone_names, export: bool = False, **network_kwargs):
             if 'None': use all known shearzones.
         export : bool
             Export network to vtk.
+        path : pathlib.Path or str
+            Path/to/01BasicInputData/
+            Alternatively, provide 'windows' or 'linux' default paths for each system.
         network_kwargs : kwargs
-            export kwargs
+            domain : dict
+                keys 'xmin', 'xmax', etc. of domain boundaries.
+
     """
+    if isinstance(path, str):
+        if path == 'linux':
+            _root = Path.cwd()  # should be path/to/mastersproject/src/mastersproject
+        elif path == 'windows':
+            _root = Path('C:/Users/Haakon/OneDrive/Dokumenter/FORSKNING/mastersproject/src/mastersproject')
+        else:
+            raise ValueError("Invalid input path type")
+        path = _root / 'GTS/01BasicInputData'
+
     if isinstance(shearzone_names, str):
         shearzone_names = [shearzone_names]
     elif shearzone_names is None:
         shearzone_names = ['S1_1', 'S1_2', 'S1_3', 'S3_1', 'S3_2']
     assert (isinstance(shearzone_names, list))
 
-    convex = convex_plane2(shearzone_names, coord_system='gts')
-
+    convex = convex_plane2(shearzone_names, coord_system='gts', path=path)
     fractures = [pp.Fracture(
-        convex.loc(convex.shearzone == sz,
-                   (
-                       'x_proj',
-                       'y_proj',
-                       'z_proj')
-                   ).to_numpy().T for sz in shearzone_names
-    )]
+        convex.loc[convex.shearzone == sz,
+                   ('x_proj', 'y_proj', 'z_proj')].to_numpy().T
+    ) for sz in shearzone_names]
 
     network = pp.FractureNetwork3d(fractures)
 
+    # domain_default = {'xmin': -6, 'xmax': 80, 'ymin': 55, 'ymax': 150, 'zmin': 0, 'zmax': 50}
     domain = network_kwargs.get('domain', None)
     if domain is not None:
         network.impose_external_boundary(domain=domain)
@@ -156,7 +168,7 @@ def fracture_network2(shearzone_names, export: bool = False, **network_kwargs):
     name = network_kwargs.get('name', None)
     if export:
         if name is None:
-            name = 'fracture_network.vtu'
+            name = 'RIGHT_fracture_network.vtu'
         if name[-4:] != '.vtu':
             name = name + '.vtu'
         network.to_vtk(name)
