3fb4f88984db0d557073781b0c15f85a8d9c0f61
haakon-e <haakonervik@hotmail.com>
2020-01-19 21:30:49 +0100

    feat(WIP): full basic mechanics setup
    Set stress BC's (with dirichlet points for consistency)
    Set zero source
    Set basic rock properties (Young's modulus, poisson ratio, friction etc.)
    Set all parameters to porepy
    Set Exporter class
    Overwrite prepare_simulation class to set custom visualization (see above)
    export a step to custom state variables (for exporting)
    - Currently exports displacement vectors
    Save normal and tangential jumps to attribute (WORK IN PROGRESS)
    Overwrite after_newton_convergence

--- src/mastersproject/GTS/isc_modelling/mechanics.py
+++ src/mastersproject/GTS/isc_modelling/mechanics.py
@@ -85,19 +85,27 @@ class ContactMechanicsISC(ContactMechanics):
         self.scalar_scale = scales['scalar_scale']
         self.length_scale = scales['length_scale']
 
-        # Grid
-        self.gb = None
-        self.Nd = None
+        # --- PHYSICAL PARAMETERS ---
+        self.stress = stress
+        self.set_rock()
 
         # --- BOUNDARY, INITIAL, SOURCE CONDITIONS ---
         self.source_scalar_borehole_shearzone = source_scalar_borehole_shearzone
 
+        # --- FRACTURES ---
         self.shearzone_names = shearzone_names
         self.n_frac = len(self.shearzone_names)
+        # Initialize data storage for normal and tangential jumps
+        self.u_jumps_tangential = np.empty((1, self.n_frac))
+        self.u_jumps_normal = np.empty((1, self.n_frac))
 
+        # --- COMPUTATIONAL MESH ---
         self.mesh_args = mesh_args
         self.box = bounding_box
+        self.gb = None
+        self.Nd = None
 
+        # --- GTS-ISC DATA ---
         self.isc_data_path = isc_data_path
         self.isc = gts.ISCData(path=self.isc_data_path)
 
@@ -185,12 +193,16 @@ class ContactMechanicsISC(ContactMechanics):
 
     def bc_type(self, g):
         """
-        We set Neumann values on all but the fracture faces,
-        which are fixed to ensure a unique solution.
+        We set Neumann values on all but a few boundary faces. Fracture faces also set to Dirichlet.
+
+        Three boundary faces (see method faces_to_fix(self, g)) are set to 0 displacement (Dirichlet).
+        This ensures a unique solution to the problem.
+        Furthermore, the fracture faces are set to 0 displacement (Dirichlet).
         """
+
         all_bf, *_ = self.domain_boundary_sides(g)
         faces = self.faces_to_fix(g)
-        bc = pp.BoundaryConditionVectorial(g, faces, ["dir"]*g.num_faces)
+        bc = pp.BoundaryConditionVectorial(g, faces, ["dir"]*len(faces))
         fracture_faces = g.tags["fracture_faces"]
         bc.is_neu[:, fracture_faces] = False
         bc.is_dir[:, fracture_faces] = True
@@ -225,38 +237,230 @@ class ContactMechanicsISC(ContactMechanics):
         return stress_eucl
 
     def bc_values(self, g):
-        """ Stress values as ISC
-        Credits: PorePy paper"""
-        # Retrieve the boundaries where values are assigned
-        all_bf, east, west, north, south, top, bottom = self.domain_boundary_sides(g)
-        A = g.face_areas
-        # Domain centred at 480 m below surface
+        """ Mechanical stress values as ISC
+        """
+        # Retrieve the domain boundary
+        all_bf, *_ = self.domain_boundary_sides(g)
 
-        # Gravity acceleration
-        gravity = (
-                pp.GRAVITY_ACCELERATION
-                * self.rock.DENSITY
-                * self._depth(g.face_centers)
-                / self.scalar_scale
-        )
+        # Get outward facing normal vectors for domain boundary, weighted for face area
+        # 1. Get normal vectors on the boundary
+        bf_normals = g.face_normals[:, all_bf]
+        # 2. Adjust direction so they face outwards
+        flip_normal_to_outwards = np.where(g.cell_face_as_dense()[0, all_bf] >= 0, 1, -1)
+        outward_normals = bf_normals * flip_normal_to_outwards
+
+        # Boundary values
         bc_values = np.zeros((g.dim, g.num_faces))
-        # TODO: Compute the actual (unperturbed) stress tensor
-        we, sn, bt = 9.2 * pp.MEGA * pp.PASCAL, 8.7 * pp.MEGA * pp.PASCAL, 13.1 * pp.MEGA * pp.PASCAL
 
-        # we = sn = bt = 9
-        bc_values[0, west] = (we * gravity[west]) * A[west]
-        bc_values[0, east] = -(we * gravity[east]) * A[east]
-        bc_values[1, south] = (sn * gravity[south]) * A[south]
-        bc_values[1, north] = -(sn * gravity[north]) * A[north]
-        if self.Nd > 2:
-            bc_values[2, bottom] = (bt * gravity[bottom]) * A[bottom]
-            bc_values[2, top] = -(bt * gravity[top]) * A[top]
+        bf_stress = np.dot(self.stress, outward_normals)
+        bc_values[:, all_bf] = bf_stress
 
         faces = self.faces_to_fix(g)
         bc_values[:, faces] = 0
 
         return bc_values.ravel("F")
 
+    def source(self, g):
+        """
+        """
+        return np.zeros(self.Nd * g.num_cells)
+
+    def set_rock(self):
+        """ Set rock properties of the ISC rock.
+        """
+
+        class GrimselGranodiorite(pp.UnitRock):
+            def __init__(self):
+                super().__init__()
+
+                self.PERMEABILITY = 1
+                self.THERMAL_EXPANSION = 1
+                self.DENSITY = 2700 * pp.KILOGRAM / (pp.METER) ** 3
+                self.POROSITY = 1
+
+                # Lamé parameters
+                self.YOUNG_MODULUS = 49 * pp.GIGA * pp.PASCAL  # Krietsch et al 2018 (Data Descriptor) - Dynamic E
+                self.POISSON_RATIO = 0.32  # Krietsch et al 2018 (Data Descriptor) - Dynamic Poisson
+                self.LAMBDA, self.rock.MU = pp.params.rock.lame_from_young_poisson(
+                    self.YOUNG_MODULUS, self.rock.POISSON_RATIO
+                )
+
+                self.FRICTION_COEFFICIENT = 0.8
+                self.POROSITY = 0.7 / 100
+
+        self.rock = GrimselGranodiorite()
+
+    def set_mu(self, g):
+        """ Set mu
+
+        Set mu in linear elasticity stress-strain relation.
+        stress = mu * trace(eps) + 2 * lam * eps
+
+        Assumes self.set_rock() is called
+        """
+        return np.ones(g.num_cells) * self.rock.MU
+
+    def set_lam(self, g):
+        """ Set lambda
+
+        Set lambda in linear elasticity stress-strain relation.
+        stress = mu * trace(eps) + 2 * lam * eps
+
+        Assumes self.set_rock() is called
+        """
+        return np.ones(g.num_cells) * self.rock.LAMBDA
+
+    def _set_friction_coefficient(self, g):
+        """ The friction coefficient is uniform, and equal to 1.
+
+        Assumes self.set_rock() is called
+        """
+        return np.ones(g.num_cells) * self.rock.FRICTION_COEFFICIENT
+
+    def set_parameters(self):
+        """
+        Set the parameters for the simulation.
+        """
+        gb = self.gb
+
+        for g, d in gb:
+            if g.dim == self.Nd:
+                # Rock parameters
+                lam = self.set_lam(g)
+                mu = self.set_mu(g)
+                C = pp.FourthOrderTensor(mu, lam)
+
+                # BC and source values
+                bc = self.bc_type(g)
+                bc_val = self.bc_values(g)
+                source_val = self.source(g)
+
+                pp.initialize_data(
+                    g,
+                    d,
+                    self.mechanics_parameter_key,
+                    {
+                        "bc": bc,
+                        "bc_values": bc_val,
+                        "source": source_val,
+                        "fourth_order_tensor": C,
+                        # "max_memory": 7e7,
+                        # "inverter": python,
+                    },
+                )
+
+            elif g.dim == self.Nd - 1:
+                friction = self._set_friction_coefficient(g)
+                pp.initialize_data(
+                    g,
+                    d,
+                    self.mechanics_parameter_key,
+                    {"friction_coefficient": friction},
+                )
+        for _, d in gb.edges():
+            mg = d["mortar_grid"]
+            pp.initialize_data(mg, d, self.mechanics_parameter_key)
+
+    def set_viz(self):
+        """ Set exporter for visualization """
+        self.viz = pp.Exporter(self.gb, name=self.file_name, folder=self.viz_folder_name)
+        # list of time steps to export with visualization.
+
+        self.u_exp = 'u_exp'
+        self.traction_exp = 'traction_exp'
+        self.export_fields = [
+            self.u_exp,
+            # self.traction_exp,
+        ]
+
+    def prepare_simulation(self):
+        """ Is run prior to a time-stepping scheme. Use this to initialize
+        discretizations, linear solvers etc.
+        """
+
+        self.create_grid()
+        self.Nd = self.gb.dim_max()
+        self.set_parameters()
+        self.assign_variables()
+        self.assign_discretizations()
+        self.initial_condition()
+        self.discretize()
+        self.initialize_linear_solver()
+
+        self.set_viz()
+
+    def export_step(self):
+        """ Export a step
+
+        Inspired by Keilegavlen 2019 (code)
+        # TODO: Export traction (see contact_mechanics_biot)
+        """
+        gb = self.gb
+        Nd = self.Nd
+
+        for g, d in gb:
+            if g.dim == Nd:  # On matrix
+                u = d[pp.STATE][self.displacement_variable].reshape((Nd, -1), order='F').copy() * self.length_scale
+
+                if g.dim != 3:  # Only called if solving a 2D problem
+                    u = np.vstack(u, np.zeros(u.shape[1]))
+
+                d[pp.STATE][self.u_exp] = u
+
+            else:  # In fractures or intersection of fractures (etc.)
+                g_h = gb.node_neighbors(g, only_higher=True)[0]  # Get the higher-dimensional neighbor
+                if g_h.dim == Nd:  # In a fracture
+                    data_edge = gb.edge_props((g, g_h))
+                    u_mortar_local = self.reconstruct_local_displacement_jump(
+                        data_edge=data_edge, from_iterate=True).copy()
+                    u_mortar_local = u_mortar_local * self.length_scale
+
+                    if g.dim != 2:  # Only called if solving a 2D problem (i.e. this is a 0D fracture intersection)
+                        u_mortar_local = np.vstack(u_mortar_local, np.zeros(u_mortar_local.shape[1]))
+
+                    d[pp.STATE][self.u_exp] = u_mortar_local
+
+        self.viz.write_vtk(data=self.export_fields, time_dependent=False)  # Write visualization
+        self.save_data()
+
+    def save_data(self):
+        """ Save normal and tangential jumps to a class attribute
+        Inspired by Keilegavlen 2019 (code)
+        """
+        gb = self.gb
+        Nd = self.Nd
+        n = self.n_frac
+        tangential_u_jumps = np.zeros((1, n))
+        normal_u_jumps = np.zeros((1, n))
+
+        for g, d in gb:
+            if g.dim < Nd:
+                g_h = gb.node_neighbors(g, only_higher=True)  # Get higher-dimensional neighbor
+                if not g_h.dim == Nd:
+                    continue  # We only consider fractures (not intersections of fractures)
+                data_edge = gb.edge_props((g, g_h))
+                u_mortar_local = self.reconstruct_local_displacement_jump(
+                    data_edge=data_edge, from_iterate=True).copy() * self.length_scale
+
+                # Jump distances
+                tangential_jump = np.linalg.norm(u_mortar_local[:-1, :], axis=0)
+                normal_jump = np.linalg.norm(u_mortar_local[-1, :])
+
+                # Ad-hoc average normal and tangential jump "estimates"
+                # TODO: Find a proper way to express the "total" displacement of a fracture
+                avg_tangential_jump = np.sum(np.abs(tangential_jump) * g.cell_volumes) / np.sum(g.cell_volumes)
+                avg_normal_jump = np.sum(tangential_jump * g.cell_volumes) / np.sum(g.cell_volumes)
+
+
+
+
+
+
+
+    def after_newton_convergence(self, solution, errors, iteration_counter):
+        """ What to do at the end of a step."""
+        self.assembler.distribute_variable(solution)
+        self.export_step()
 
 class ContactMechanicsIsotropicISC(IsotropicSetup):
     def __init__(self, **kwargs):
