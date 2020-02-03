39f07ff793e1264da137cfc40e027ea2ccbe385d
Haakon Ludvig Langeland Ervik <haakonervik@hotmail.com>
2020-02-03 16:38:45 +0100

    init: update init

--- src/mastersproject/refinement/__init__.py
+++ src/mastersproject/refinement/__init__.py
@@ -1,4 +1,14 @@
-from refinement.refinement import refine_mesh, \
-    coarse_fine_cell_mapping, \
-    gb_coarse_fine_cell_mapping
 
+# Refinement methods
+from refinement.refinement import (
+    refine_mesh,
+    coarse_fine_cell_mapping,
+    gb_coarse_fine_cell_mapping,
+)
+
+
+# Testing
+from refinement.tests.test_refinement import (
+    test_refine,
+    test_refine_n_times,
+)
