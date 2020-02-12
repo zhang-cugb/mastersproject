289163b0acf285d146ecaa4560bc1f1e9eca49e6
Haakon Ludvig Langeland Ervik <haakonervik@hotmail.com>
2020-02-12 11:04:44 +0100

    logging

--- src/mastersproject/util/logging_util.py
+++ src/mastersproject/util/logging_util.py
@@ -30,3 +30,24 @@ def trace(f, *args, **kw):
     kwstr = ', '.join('%r: %r' % (k, kw[k]) for k in sorted(kw))
     logging.debug(f"Calling {f.__name__} with args {args}, {{{kwstr}}}")
     return f(*args, **kw)
+
+
+def __setup_logging(path, log_fname="results.log"):
+    path = str(path)
+    # GTS logger
+    gts_logger = logging.getLogger('GTS')
+    gts_logger.setLevel(logging.INFO)
+
+    # PorePy logger
+    pp_logger = logging.getLogger('porepy')
+    pp_logger.setLevel(logging.DEBUG)
+
+    # Add handler for logging debug messages to file.
+    fh = logging.FileHandler(path + "/" + log_fname)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+
+    gts_logger.addHandler(fh)
+    pp_logger.addHandler(fh)
+
+
