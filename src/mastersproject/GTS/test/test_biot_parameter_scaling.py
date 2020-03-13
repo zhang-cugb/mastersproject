c3d90be02774f6750c82781927703b9390f99644
Haakon Ludvig Langeland Ervik <haakonervik@hotmail.com>
2020-03-13 14:12:01 +0100

    test: improve condition number test

--- src/mastersproject/GTS/test/test_biot_parameter_scaling.py
+++ src/mastersproject/GTS/test/test_biot_parameter_scaling.py
@@ -20,6 +20,7 @@ from pathlib import Path
 import porepy as pp
 import numpy as np
 from porepy.models.contact_mechanics_model import ContactMechanics
+import pendulum
 
 import GTS as gts
 from .test_create_grid import test_create_grid
@@ -202,67 +203,51 @@ def test_biot_parameter_scaling(**kw):
     return setup, mech, flow, scaled_mech, scaled_flow
 
 
-def test_biot_condition_number(**kw):
+def test_biot_condition_number(
+        test_name: str,
+        ls: float,
+        ss: float,
+        shearzones: List[str] = None,
+):
     """ Test scaling of parameters in Biot
+
+    Note: No problem is solved. Condition number is simply computed.
+
+    Parameters
+    ----------
+    test_name : str
+        name of this test
+    ls, ss : float
+        length scale and scalar scale, respectively
+    shearzones : List[str] (Default: None)
+        list of shearzones
     """
-    _this_file = Path(os.path.abspath(__file__)).parent
-    _results_path = _this_file / "results/test_biot_condition_number/default"
-    _results_path.mkdir(parents=True, exist_ok=True)  # Create path if not exists
-    __setup_logging(_results_path)
-    # path = str(_results_path)
-    # # GTS logger
-    # gts_logger = logging.getLogger('GTS')
-    # gts_logger.setLevel(logging.INFO)
-    #
-    # # PorePy logger
-    # pp_logger = logging.getLogger('porepy')
-    # pp_logger.setLevel(logging.DEBUG)
-    #
-    # # Add handler for logging debug messages to file.
-    # fh = logging.FileHandler(path + "/" + "results.log")
-    # fh.setLevel(logging.DEBUG)
-    # fh.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
-    #
-    # gts_logger.addHandler(fh)
-    # pp_logger.addHandler(fh)
 
-    logger.info(f"Path to results: {_results_path}")
-
-    # --- DOMAIN ARGUMENTS ---
+    # Custom parameters
     params = {
-        'mesh_args':
-            {'mesh_size_frac': 10, 'mesh_size_min': .1*10, 'mesh_size_bound': 6*10},
-        'bounding_box':
-            {'xmin': -20, 'xmax': 80, 'ymin': 50, 'ymax': 150, 'zmin': -25, 'zmax': 75},
-        'shearzone_names':
-            ["S1_1", "S1_2", "S1_3", "S3_1", "S3_2"],
-        'folder_name':
-            _results_path,
-        'solver':
-            'direct',
-        'stress':
-            gts.isc_modelling.stress_tensor(),
-        'source_scalar_borehole_shearzone':
-            {'borehole': 'INJ1', 'shearzone': 'S1_1'},
-        'length_scale':
-            kw.get('ls', 100),
-        'scalar_scale':
-            kw.get('ss', 1 * pp.GIGA),
+        "shearzone_names": shearzones,
+        "length_scale": ls,
+        "scalar_scale": ss,
     }
-    logger.info(f"input parameters: \n {params}")
 
+    # Storage folder
+    this_method_name = test_biot_condition_number.__name__
+    now_as_YYMMDD = pendulum.now().format("YYMMDD")
+    shearzone_str = str(shearzones) if not shearzones else "-".join(shearzones)
+    scale_str = f"ss{ss}_ls{ls}"
+    _folder_root = f"{this_method_name}/{now_as_YYMMDD}/{test_name}/{shearzone_str}/{scale_str}"
+
+    params = test_util.prepare_params(
+        path_head=_folder_root,
+        params=params,
+        setup_loggers=True,
+    )
+
+    # Set up model class
     setup = gts.ContactMechanicsBiotISC(params)
-
-    setup.create_grid(overwrite_grid=True)
-    ss = setup.scalar_scale
-    ls = setup.length_scale
-    print(f'ss={ss}')
-    print(f'ls={ls}')
-
-    # Recompute parameters
     setup.prepare_simulation()
 
-    # Mimic NewtonSolver:
+    # Mimic NewtonSolver: --> This can probably be removed
     setup.before_newton_iteration()
 
     # Check size of entries in matrix A.
@@ -270,7 +255,7 @@ def test_biot_condition_number(**kw):
     logger.info("------------------------------------------")
     logger.info(f"Max element in A {np.max(np.abs(A)):.2e}")
     logger.info(f"Max {np.max(np.sum(np.abs(A), axis=1)):.2e} and min {np.min(np.sum(np.abs(A), axis=1)):.2e} A sum.")
-
+    logger.info(f"Length scale: {setup.length_scale:.1e}. Scalar scale: {setup.scalar_scale:.1e}.")
     return setup
 
 
